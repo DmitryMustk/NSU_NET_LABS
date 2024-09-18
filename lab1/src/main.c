092c8964fe0ae0ce9d01afbd83f82e90e2216d69
DmitryMustk <dmutrymust@gmail.com>
2024-09-19 01:09:58 +0700

    [lab1] some more refactor+decomposing shit

--- lab1/src/main.c
+++ lab1/src/main.c
@@ -1,4 +1,5 @@
 #include <asm-generic/socket.h>
+#include <bits/types/struct_timeval.h>
 #include <netinet/in.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -22,10 +23,13 @@
 #define PEEK_IP_ADRESS "8.8.8.8"
 
 //TODO: add timer
-//TODO: check if there is a need to create mcAdress a second time
+//TODO: check if there is a need to create mcAddress a second time
 //TODO: dynamic output
 //TODO: IPv6 support
 //TODO: user input address support
+
+
+void sendAndRecvMessages(int writeSocket, int readSocket, const struct sockaddr_in* sendAddr, const char* addressStr);
 
 int createSocket(int domain, int type, int protocol) {
 	int sockfd = socket(domain, type, protocol);
@@ -51,7 +55,7 @@ void bindSocket(int sockfd, const char* address, in_port_t port) {
 	addr.sin_family = AF_INET;
 	addr.sin_port = htons(port);
 	if (inet_pton(AF_INET, address, &addr.sin_addr) <= 0) {
-		handleError("Invalid adress");
+		handleError("Invalid address");
 	}
 	if (bind(sockfd, (struct sockaddr*)&addr, sizeof(addr)) != 0) {
 		handleError("Can't bind socket");
@@ -59,35 +63,34 @@ void bindSocket(int sockfd, const char* address, in_port_t port) {
 }
 
 void autoBindSocket(int sockfd) {
-	struct sockaddr_in peekAdress;
-	peekAdress.sin_family = AF_INET;
-	peekAdress.sin_port = htons(PORT);
-	if (inet_pton(AF_INET, PEEK_IP_ADRESS, &peekAdress.sin_addr) <= 0) {
-		handleError("Invalid peekAdress");
+	struct sockaddr_in peekAddress;
+	peekAddress.sin_family = AF_INET;
+	peekAddress.sin_port = htons(PORT);
+	if (inet_pton(AF_INET, PEEK_IP_ADRESS, &peekAddress.sin_addr) <= 0) {
+		handleError("Invalid peekAddress");
 	}
 
 	const char* message = "PEEK_MESSAGE";
-	sendto(sockfd, message, strlen(message), 0, (struct sockaddr*)&peekAdress, sizeof(peekAdress));
+	sendto(sockfd, message, strlen(message), 0, (struct sockaddr*)&peekAddress, sizeof(peekAddress));
 }
 
-int getAdressStr(struct sockaddr_in address, char* strBuf, size_t bufSize) {
+int getAddressStr(struct sockaddr_in address, char* strBuf, size_t bufSize) {
 	//2 = ':' + '\0'
 	if (bufSize < INET_ADDRSTRLEN + PORT_LEN + 1) {
-		printf("faf");
 		return -1;
 	}
 
 	char ipStr[INET_ADDRSTRLEN];
 	inet_ntop(AF_INET, &address.sin_addr, ipStr, sizeof(ipStr));
 	snprintf(strBuf, bufSize, "%s:%d", ipStr, ntohs(address.sin_port));
-//	printf("ADRESS %s", strBuf);
+	//strBuf[INET_ADDRSTRLEN + PORT_LEN + 1] = '\0';
 	return 0;
 }
 
-void joinMCGroup(int sockfd, const char* adress) {
+void joinMCGroup(int sockfd, const char* address) {
 	struct ip_mreq mreq;
-	if (inet_aton(adress, &mreq.imr_multiaddr) == 0) {
-		handleError("Invalid mcAdress");
+	if (inet_aton(address, &mreq.imr_multiaddr) == 0) {
+		handleError("Invalid mcAddress");
 	}
 	mreq.imr_interface.s_addr = htonl(INADDR_ANY);
 	if (setsockopt(sockfd, IPPROTO_IP, IP_ADD_MEMBERSHIP, &mreq, sizeof(mreq)) != 0) {
@@ -101,46 +104,41 @@ void getSocketName(int sockfd, struct sockaddr_in* addr, socklen_t* addrSize) {
 	}
 } 
 
-int main(void) {
-	const char* mcAdress = "224.0.0.5";
+void configureAddress(struct sockaddr_in* addr, const char* address, in_port_t port) {
+	bzero(addr, sizeof(*addr));
+	addr->sin_family = AF_INET;
+	addr->sin_port = htons(port);
 
-	int readSocket = createSocket(AF_INET, SOCK_DGRAM, 0); 
-	setSocketReuse(readSocket);	
-	bindSocket(readSocket, mcAdress, PORT);	
-
-	joinMCGroup(readSocket, mcAdress);	
-
-
-	int writeSocket = createSocket(AF_INET, SOCK_DGRAM, 0); 
-
-	struct sockaddr_in sendAddr;
-	bzero(&sendAddr, sizeof(sendAddr));
-	sendAddr.sin_family = AF_INET;
-	sendAddr.sin_port = htons(PORT);
-	if (inet_pton(AF_INET, mcAdress, &sendAddr.sin_addr) <= 0) {
-		handleError("Invalid send adress");
+	if (address && inet_pton(AF_INET, address, &addr->sin_addr) <= 0) {
+		handleError("Invalid address");
 	}
+}
 
-	//GET UNIQUE ID OF PROCESS; ID = IP + PORT of writeSocket
-	autoBindSocket(writeSocket);
+void initSocket(int* sockfd, const char* address, in_port_t port) {
+	*sockfd = createSocket(AF_INET, SOCK_DGRAM, 0);
+	setSocketReuse(*sockfd);
 
-	struct sockaddr_in uniqueAddress;
-	socklen_t uniqueAddressLen = sizeof(uniqueAddress);
-	getSocketName(writeSocket, &uniqueAddress, &uniqueAddressLen);
-		
-	size_t addressBufLen = INET_ADDRSTRLEN + PORT_LEN + 1;
-	char adressStrBuf[addressBufLen];
-	getAdressStr(uniqueAddress, adressStrBuf, addressBufLen);
+	if (address != NULL) {
+		bindSocket(*sockfd, address, port);
+	}
+}
 
+void handleSocketCommunication(int readSocket, int writeSocket, const char* mcAddress, const char* uniqueAddressStr) {
+	struct sockaddr_in sendAddr;
+	configureAddress(&sendAddr, mcAddress, PORT);
+
+	sendAndRecvMessages(writeSocket, readSocket, &sendAddr, uniqueAddressStr);
+}
+
+void sendAndRecvMessages(int writeSocket, int readSocket, const struct sockaddr_in* sendAddr, const char* addressStr) {
 	ssize_t responseLen;
 	char responseBuf[BUF_SIZE];
 
-	//Timeout for select
+	fd_set readFds;
+	
 	struct timeval timeout;
 	timeout.tv_sec = 1;
 	timeout.tv_usec = 0;
-
-	fd_set readFds;
 
 	while (1) {
 		FD_ZERO(&readFds);
@@ -149,23 +147,46 @@ int main(void) {
 		int activity = select(readSocket + 1, &readFds, NULL, NULL, &timeout);
 		if (activity < 0) {
 			handleError("select error");
-		} else if (activity != 0) {
-			if (FD_ISSET(readSocket, &readFds)) {
-				responseLen = recvfrom(readSocket, responseBuf, BUF_SIZE, 0, NULL, NULL);
-				if (responseLen < 0) {
-					handleError("Failed to receive message");
-				}
-				if (strcmp(responseBuf, adressStrBuf) != 0) {
-					printf("DETECTED COPY: %s\n", responseBuf);
-				} 
-
+		} else if (activity != 0 && FD_ISSET(readSocket, &readFds)) {
+			responseLen = recvfrom(readSocket, responseBuf, BUF_SIZE, 0, NULL, NULL);
+			responseBuf[responseLen] = '\0';
+			if (responseLen < 0) {
+				handleError("Failed to receive message");
 			}
+
+			if (strcmp(responseBuf, addressStr) != 0) {
+				printf("STRCMP RES: %d", strcmp(responseBuf, addressStr));
+				printf("DETECTED COPY: %s\n", responseBuf);
+			} 
 		}
 
-		sendto(writeSocket, adressStrBuf, strlen(adressStrBuf), 0, (struct sockaddr*)&sendAddr, sizeof(sendAddr));
+		sendto(writeSocket, addressStr, strlen(addressStr), 0, (struct sockaddr*)sendAddr, sizeof(*sendAddr));
 		sleep(1);
-		
+
 	}
+}
+
+int main(void) {
+	const char* mcAddress = "224.0.0.5";
+	int readSocket, writeSocket;
+
+	initSocket(&readSocket, mcAddress, PORT);
+	joinMCGroup(readSocket, mcAddress);
+
+	initSocket(&writeSocket, NULL, 0);
+	autoBindSocket(writeSocket);
+
+	struct sockaddr_in uniqueAddress;
+	socklen_t uniqueAddressLen = sizeof(uniqueAddress);
+	getSocketName(writeSocket, &uniqueAddress, &uniqueAddressLen);
+
+	char addressStr[INET6_ADDRSTRLEN + PORT_LEN + 1];
+	getAddressStr(uniqueAddress, addressStr, sizeof(addressStr));
+
+	printf("MY ADDRESS: %s\n", addressStr);
+	handleSocketCommunication(readSocket, writeSocket, mcAddress, addressStr);
+
+		
 	close(writeSocket);
 	close(readSocket);
 	return 0;
