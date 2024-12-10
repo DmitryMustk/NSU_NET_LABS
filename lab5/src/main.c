65a3fdc8b9d91ae74f561c55c577522dfd623dc8
DmitryMustk <dmutrymust@gmail.com>
2024-12-11 06:09:08 +0700

    [lab5] minor changes

--- lab5/src/main.c
+++ lab5/src/main.c
@@ -12,21 +12,21 @@ int main(int argc, char** argv) {
 	Logger* log = createLogger("log.txt", LOG_DEBUG, 1);
 	if (argc != 2) {
 		logMessage(log, LOG_ERROR, "Usage: %s <port>", argv[0]);
-		return EXIT_FAILURE;
+		return -1;
 	}
 	
 	in_port_t port = htons(atoi(argv[1]));
 	if (ntohs(port) != atoi(argv[1]) || atoi(argv[1]) == 0) {
 		logMessage(log, LOG_ERROR, "Port value must be between (0, %d)", MAX_PORT_VALUE);
-		return EXIT_FAILURE;
+		return -1;
 	}
 
 	logMessage(log, LOG_INFO, "socksProxy started on %s port...", argv[1]); 
-	logMessage(log, LOG_INFO, "NoAuth required");
+	logMessage(log, LOG_INFO, "No Auth method required");
 
 	if (startServer(port, log) < 0) {
 		logMessage(log, LOG_ERROR, "Failed to start server");
-		return EXIT_FAILURE;
+		return 1-;
 	}
 
 	closeLogger(log);
